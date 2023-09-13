75d78bb1f66cb60cc6ebc2e30411ca3f7931becc
John Yakovl <tchuwipeach@yandex.com>
2023-09-13 20:36:42 +0700

    commit

--- altobellos/urls.py
+++ altobellos/urls.py
@@ -1,14 +1,5 @@
 from django.contrib import admin
 from django.urls import path
-from django.http import HttpResponse
-
-
-def home(request):
-    return HttpResponse('home page')
-
-
-def room(request):
-    return HttpResponse('room page')
 
 
 urlpatterns = [
