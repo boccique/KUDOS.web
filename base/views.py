244e49b37165a5e6b7fccdebec0aa50daaecf9f4
John Yakovl <tchuwipeach@yandex.com>
2023-09-29 00:04:06 +0700

    3:10

--- base/views.py
+++ base/views.py
@@ -82,7 +82,8 @@ def home(request):
 
 def room(request, pk):
     room = Room.objects.get(id=pk)
-    contex = {'room': room}
+    room_messages = room.message_set.all().order_by('-created')
+    contex = {'room': room, 'room_messages': room_messages}
     return render(request, 'base/room.html', contex)
 
 
