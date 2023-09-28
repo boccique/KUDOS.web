73499e76f7fdc03044bfa8e059dc3fa7ca8be166
John Yakovl <tchuwipeach@yandex.com>
2023-09-28 23:36:20 +0700

    ,,l

--- base/views.py
+++ base/views.py
@@ -16,6 +16,7 @@ from django.http import HttpResponse
 # ]
 
 def loginPage(request):
+    page = 'login'
     if request.user.is_authenticated:
         return redirect('home')
 
@@ -36,13 +37,17 @@ def loginPage(request):
         else:
             messages.error(request, "Username OR password does not exist")
 
-    context = {}
+    context = {'page': page}
     return render(request, 'base/login_register.html', context)
 
 
 def logoutUser(request):
     logout(request)
     return redirect('home')
+
+def registerPage(request):
+    page = 'register'
+    return render(request, 'base/login_register.html')
 
 
 def home(request):
