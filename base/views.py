c8f4b34925306c06ff45f6d6c909a2e77ed29436
John Yakovl <tchuwipeach@yandex.com>
2023-09-28 23:50:21 +0700

    323

--- base/views.py
+++ base/views.py
@@ -7,6 +7,7 @@ from .forms import RoomForm
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
 
 
 # rooms = [
@@ -21,7 +22,7 @@ def loginPage(request):
         return redirect('home')
 
     if request.method == 'POST':
-        username = request.POST.get('username')
+        username = request.POST.get('username').lower()
         password = request.POST.get('password')
 
         try:
@@ -45,9 +46,22 @@ def logoutUser(request):
     logout(request)
     return redirect('home')
 
+
 def registerPage(request):
-    page = 'register'
-    return render(request, 'base/login_register.html')
+    form = UserCreationForm()
+
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'An error occurred during registration')
+
+    return render(request, 'base/login_register.html', {'form': form})
 
 
 def home(request):
