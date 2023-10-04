46c9f23d5621cc181ad829aeb5167b2d8a1284c0
John Yakovl <tchuwipeach@yandex.com>
2023-10-04 23:45:48 +0700

    commit

--- base/views.py
+++ base/views.py
@@ -83,7 +83,7 @@ def home(request):
 def room(request, pk):
     room = Room.objects.get(id=pk)
     room_messages = room.message_set.all().order_by('-created')
-
+    participants = room.participants.all()
 
     if request.method == "POST":
         message = Message.objects.create(
@@ -91,9 +91,10 @@ def room(request, pk):
             room=room,
             body=request.POST.get('body')
         )
+        room.participants.add(request.user)
         return redirect('room', pk=room.id)
 
-    contex = {'room': room, 'room_messages': room_messages}
+    contex = {'room': room, 'room_messages': room_messages, 'participants': participants}
     return render(request, 'base/room.html', contex)
 
 
