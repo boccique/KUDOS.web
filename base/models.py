9dad31fd43f19673e6a732e4adb2907c51ce5797
John Yakovl <tchuwipeach@yandex.com>
2023-10-05 20:30:55 +0700

    commit

--- base/models.py
+++ base/models.py
@@ -32,5 +32,8 @@ class Message(models.Model):
     updated = models.DateField(auto_now=True)
     created = models.DateField(auto_now_add=True)
 
+    class Meta:
+        ordering = ['-updated', '-created']
+
     def __str__(self):
         return self.body[0:50]
