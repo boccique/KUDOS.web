f183d665d7b0fa8bb1eba56052c9b24986c1c6aa
John Yakovl <tchuwipeach@yandex.com>
2023-10-10 21:38:37 +0700

    commit

--- altobellos/settings.py
+++ altobellos/settings.py
@@ -122,7 +122,11 @@ STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
+
 ]
+# STATIC_ROOt =
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
